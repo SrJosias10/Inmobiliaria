3be2dcbe1fed1e46d0909a2c745afb3617f3e86e
Josias Omar Olave <mromarolave@gmail.com>
2024-10-27 23:25:05 -0300

    Primer commit

---
+++ Dominio/Inmueble.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class Inmueble
+    {
+        public int ID { get; set; }
+        public TipoInmueble Tipo { get; set; }
+        public Ubicacion Ubicacion { get; set; }
+        public Estado Estado { get; set; }
+        public Moneda Moneda { get; set; }
+        public string Descripcion { get; set; }
+        public float Precio { get; set; }
+        public int Ambientes { get; set; }
+        public int Garages { get; set; }
+        public int Dormitorios { get; set; }
+        public int Banos { get; set; }
+        public int Antiguedad { get; set; }
+        public int Expensas { get; set; }
+        public int Superficie { get; set; }
+    }
+}
---
+++ WebApplication/Inicio.aspx
@@ -0,0 +1,11 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPrincipal.Master" AutoEventWireup="true" CodeBehind="Inicio.aspx.cs" Inherits="WebApplication.Inicio" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <!-- Titulo -->
+    <div class="tituloinicio">
+        <h1>Bienvenidos a Prime Properties</h1>
+    </div>
+    <!-- Buscador -->
+    <!-- Carrusel -->
+</asp:Content>
